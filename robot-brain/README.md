9be61529672494c86b47c7e84290845f25f77825
schimen <einesimen@gmail.com>
2021-10-29 18:29:22 +0200

    killer-robot: Added functionality for backup radio module

--- robot-brain/README.md
+++ robot-brain/README.md
@@ -1,4 +1,5 @@
 # Brain of our killer robot
 
 This brain shall receive our evil commands and execute them.
-We use [this library](https://playground.arduino.cc/Code/SimpleTimer/) for timers for non blocking delay.
+We use [this library](https://playground.arduino.cc/Code/SimpleTimer/) for timers for non blocking delay. This library is included in repo so there is no need to install it.
+We also use [this library](https://www.airspayce.com/mikem/arduino/RadioHead/) for the 433MHz radio, so be sure to install it.
--- robot-brain/robot-brain.ino
+++ robot-brain/robot-brain.ino
@@ -1,3 +1,5 @@
+#include <RH_ASK.h>
+#include <SPI.h>
 #include "SimpleTimer.h"
 #include "Motor.h"
 
@@ -19,27 +21,75 @@ const uint8_t motor2Pin2   = 9;
 const uint16_t stepDelay = 50;
 const uint8_t  stepSize  = 10;
 
+// backup radio (433MHz module)
+RH_ASK backupRadio;
+const uint8_t bufLen = 4;
+char receiveBuffer[bufLen];
+
+// timers for acceleration control
 SimpleTimer timer1;
 SimpleTimer timer2;
-
+// objects for motor control
 Motor motor1(1, motor1Pin1, motor1Pin2, motor1Enable, stepDelay, stepSize, &timer1);
 Motor motor2(2, motor2Pin1, motor2Pin2, motor2Enable, stepDelay, stepSize, &timer2);
 
 void setup() {
   Serial.begin(9600);
+  backupRadio.init();
+  resetBuffer();
 }
 
 void loop() {
+  
   timer1.run();
   timer2.run();
+  backupRadio.recv(receiveBuffer, &bufLen);
+  if (newRadioMessage()) {
+    // save new message in seperate buffer
+    char validMessage[bufLen];
+    memcpy(validMessage, receiveBuffer, bufLen);
+    
+    uint8_t command = validMessage[0];
+    int8_t  value   = validMessage[2];
+    handleNewMessage(command, value);
+  }
+  
   while (Serial.available() > 0) {
-    int command = Serial.parseInt();
-    int value   = Serial.parseInt();
-    if (command == MOTOR_1_COMMAND) {
-      motor1.set(value);
-    }
-    else if (command == MOTOR_2_COMMAND) {
-      motor2.set(value);
+    uint8_t command = Serial.parseInt();
+    int8_t  value   = Serial.parseInt();
+    if (Serial.read() == '\n') {
+      handleNewMessage(command, value); 
     }
   }
+}
+
+void handleNewMessage(uint8_t command, int8_t value) {
+  if (command == MOTOR_1_COMMAND) {
+    motor1.set(value);
+  }
+  else if (command == MOTOR_2_COMMAND) {
+    motor2.set(value);
+  } 
+}
+
+bool newRadioMessage() {
+  // check if there is a new valid message
+  if ((receiveBuffer[1] == ',') and (receiveBuffer[3] == '\n')) {
+    return true;
+  }
+  else {
+    // reset buffer if message is not what we want
+    for (uint8_t i; i < bufLen; i++) {
+      if ((receiveBuffer[i] == ',') or receiveBuffer[i] == '\n') {
+        resetBuffer();
+        return false;
+      }
+    }
+    return false;
+  }
+}
+
+void resetBuffer() {
+  // reset backupRadio buffer
+  memset(receiveBuffer, 0, bufLen);
 }
