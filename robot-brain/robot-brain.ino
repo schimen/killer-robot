7b4f60d359370140e81b5baed08255b25bb29d03
schimen <einesimen@gmail.com>
2021-11-03 13:21:32 +0100

    robot-brain: Added servo and got backup radio working

--- robot-brain/robot-brain.ino
+++ robot-brain/robot-brain.ino
@@ -1,21 +1,25 @@
-#include <RH_ASK.h>
+#include <RH_ASK.h> // <- this must be (globally) installed to compile this sketch
 #include <SPI.h>
 #include "SimpleTimer.h"
 #include "Motor.h"
 
-// motor commands
+// motor and servo commands
 #define MOTOR_1_COMMAND 10
 #define MOTOR_2_COMMAND 11
+#define SERVO_COMMAND   12
 
 // left motor
 const uint8_t motor1Enable = 5;
-const uint8_t motor1Pin1   = 6;
+const uint8_t motor1Pin1   = 2;
 const uint8_t motor1Pin2   = 7;
 
 // right motor
-const uint8_t motor2Enable = 10;
+const uint8_t motor2Enable = 6;
 const uint8_t motor2Pin1   = 8;
-const uint8_t motor2Pin2   = 9;
+const uint8_t motor2Pin2   = 4;
+
+// servo
+const uint8_t servoPin = 3;
 
 // acceleration settings
 const uint16_t stepDelay = 50;
@@ -24,72 +28,87 @@ const uint8_t  stepSize  = 10;
 // backup radio (433MHz module)
 RH_ASK backupRadio;
 const uint8_t bufLen = 4;
-char receiveBuffer[bufLen];
+uint8_t receiveBuffer[bufLen];
 
 // timers for acceleration control
 SimpleTimer timer1;
 SimpleTimer timer2;
+
 // objects for motor control
 Motor motor1(1, motor1Pin1, motor1Pin2, motor1Enable, stepDelay, stepSize, &timer1);
 Motor motor2(2, motor2Pin1, motor2Pin2, motor2Enable, stepDelay, stepSize, &timer2);
 
 void setup() {
-  Serial.begin(9600);
-  backupRadio.init();
-  resetBuffer();
+  Serial.begin(9600); // communication with bluetooth module
+  backupRadio.init(); // backup radio receiver
+  resetBuffer();      // reset the buffer of the radio (this is probably unecessary)
 }
 
 void loop() {
-  
+  // start both timers to keep track of the motors
   timer1.run();
   timer2.run();
-  backupRadio.recv(receiveBuffer, &bufLen);
-  if (newRadioMessage()) {
-    // save new message in seperate buffer
-    char validMessage[bufLen];
-    memcpy(validMessage, receiveBuffer, bufLen);
-    
-    uint8_t command = validMessage[0];
-    int8_t  value   = validMessage[2];
-    handleNewMessage(command, value);
-  }
-  
-  while (Serial.available() > 0) {
-    uint8_t command = Serial.parseInt();
-    int8_t  value   = Serial.parseInt();
-    if (Serial.read() == '\n') {
+  // check if there are new messages at the backup radio (buffer is full)
+  if (backupRadio.recv(receiveBuffer, &bufLen)) {
+    if (validRadioMessage()) { // check if the new message is valid (maybe the buffer should be reset)
+      // save new message in seperate buffer (this is probably also unecessary)
+      char validMessage[bufLen];
+      memcpy(validMessage, receiveBuffer, bufLen);
+      uint8_t command = validMessage[0];
+      int8_t  value   = validMessage[2];
+      // do something with the values!
       handleNewMessage(command, value); 
     }
-  }
-}
-
-void handleNewMessage(uint8_t command, int8_t value) {
-  if (command == MOTOR_1_COMMAND) {
-    motor1.set(value);
-  }
-  else if (command == MOTOR_2_COMMAND) {
-    motor2.set(value);
-  } 
-}
-
-bool newRadioMessage() {
-  // check if there is a new valid message
-  if ((receiveBuffer[1] == ',') and (receiveBuffer[3] == '\n')) {
-    return true;
-  }
-  else {
-    // reset buffer if message is not what we want
-    for (uint8_t i; i < bufLen; i++) {
-      if ((receiveBuffer[i] == ',') or receiveBuffer[i] == '\n') {
-        resetBuffer();
-        return false;
-      }
+    else { // if the message is not valid, delete it. we do not want it...
+      resetBuffer();
     }
-    return false;
   }
+  // new message from the bluetooth module
+  while (Serial.available() > 0) {
+    // parse message ("<command>,<value>\n")
+    uint8_t command = Serial.parseInt();
+    int     value   = Serial.parseInt();
+    if (Serial.read() == '\n') {
+      handleNewMessage(command, value); // do something with it!
+    }
+  }
+}
+
+int8_t limitSpeed(int value)  {
+  // limit the speed if it is outside our range
+  if (value >= 127) {
+    return 127;
+  }
+  else if (value <= -127)  {
+    return -127;
+  }
+  return value;
+}
+
+void handleNewMessage(uint8_t command, int value) {
+  // do something with the new message!
+  int8_t limitedValue = limitSpeed(value);
+  switch (command) {
+    case MOTOR_1_COMMAND:
+      motor1.set(limitedValue);
+      break;
+    case MOTOR_2_COMMAND:
+      motor2.set(limitedValue);
+      break;
+    case SERVO_COMMAND:
+      analogWrite(servoPin, abs(limitedValue)*2);
+      break;
+  }
+}
+
+bool validRadioMessage() {
+  // check if the message is valid message ("<command>,<value>\n")
+  return ((receiveBuffer[1] == ',') and (receiveBuffer[3] == '\n'));
+  // I should add message as argument here, and not use the global buffer...
 }
 
 void resetBuffer() {
   // reset backupRadio buffer
   memset(receiveBuffer, 0, bufLen);
+  // I should add buffer as argument here also...
 }
