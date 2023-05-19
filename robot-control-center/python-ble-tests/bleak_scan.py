3ec6ed58c39041b28a734da55ee539d03eca3e97
schimen <einesimen@gmail.com>
2023-05-19 17:42:36 +0000

    Add some print statements to scan program

--- robot-control-center/python-ble-tests/bleak_scan.py
+++ robot-control-center/python-ble-tests/bleak_scan.py
@@ -4,6 +4,10 @@ from bleak import BleakScanner
 async def main():
     devices = await BleakScanner.discover()
     for d in devices:
-        print(d)
+        print()
+        print(f'Name: {d.name}, address: {d.address}, RSSI: {d.rssi}')
+        print(f'Details: {d.details}')
+        print(f'Metadata: {d.metadata}')
+        print()
 
 asyncio.run(main())
